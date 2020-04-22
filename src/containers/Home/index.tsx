334e62b1f99dfe0e00029d33470ce0bff176f77a
ABHILASH <ss.abhilash3@gmail.com>
2020-04-22 21:39:45 +0530

    wip: add header component

---
+++ src/containers/Home/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import { Appbar } from 'react-native-paper';
+import Header from '../../components/Header';
+
+const Home = () => {
+  return (
+    <View>
+    <Header />
+    <Text>Home</Text>
+    </View>
+  );;
+};
+
+export default Home;
