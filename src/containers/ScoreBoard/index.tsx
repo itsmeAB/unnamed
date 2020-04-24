ceed727472d0a6dab02cb03af93d4889126f9ccd
ABHILASH <ss.abhilash3@gmail.com>
2020-04-24 21:40:12 +0530

    wip

---
+++ src/containers/ScoreBoard/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import { Appbar } from 'react-native-paper';
+import Header from '../../components/Header';
+
+const ScoreBoard = () => {
+  return (
+    <View>
+    <Header />
+    <Text>ScoreBoard</Text>
+    </View>
+  );;
+};
+
+export default ScoreBoard;
