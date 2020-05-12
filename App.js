1ba469b699593eaeb8fef62bee6dd04770ee3e1b
itsmeAB <ss.abhilash3@gmail.com>
2020-05-12 22:08:47 +0530

    wip

--- App.js
+++ App.js
@@ -8,6 +8,7 @@ import { Platform, StatusBar, StyleSheet, View } from 'react-native';
 
 import BottomTabNavigator from './navigation/BottomTabNavigator';
 import LinkingConfiguration from './navigation/LinkingConfiguration';
+import ScoreboardScreen from './screens/ScoreboardScreen';
 
 const Stack = createStackNavigator();
 
@@ -46,6 +47,7 @@ export default function App(props) {
         <NavigationContainer linking={LinkingConfiguration}>
           <Stack.Navigator>
             <Stack.Screen name="Root" component={BottomTabNavigator} />
+            <Stack.Screen name="Scoreboard" component={ScoreboardScreen} />
           </Stack.Navigator>
         </NavigationContainer>
       </View>
